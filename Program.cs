49df966507d5be63ee2b29410d799964247f8eb6
Olexander Korkots <k.sasha.g@gmail.com>
2021-03-29 17:07:56 +0300

    made all logic

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using InternshipTest.Person;
+using InternshipTest.Institution;
+using InternshipTest.Institution.InterLink;
+using System.Collections.Generic;
+
+namespace InternshipTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var s = new Student("Alex", new Knowledge(5));
+            University university = new University("CH.U.I.");
+            university.AddStudent(new Student("Andrew Kostenko", new Knowledge(4)));
+            university.AddStudent(new Student("Julia Veselkina"));
+            university.AddStudent(new Student("Maria Perechrest"));
+            university.AddStudent(s);
+
+            Internship internship = new Internship("Interlink");
+            //internship.AddStudents(university.Students);
+
+
+            internship.AddStudentsFromOtherEducPlace(university); // поліморфізм
+
+            Console.WriteLine("List of internship's students:");
+            Console.WriteLine(internship.GetStudents());
+        }
+    }
+}
